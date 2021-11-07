b589c9c27aa03a19c5db97a7fd4af6f2de733c5a
Bouncehball <93630871+Bouncehball@users.noreply.github.com>
2021-11-07 17:32:52 -0600

    Update collisions.py

--- examples/collisions.py
+++ examples/collisions.py
@@ -1,105 +1,67 @@
-import pygame, pgeng
-from pygame.locals import *
+import pymunk
+import pygame
 
-pygame.init()
 
-clock = pygame.time.Clock()
-Screen = pgeng.Screen((320, 240), vsync=0)
-display = Screen.get_display()
-small_font = pgeng.create_font((255, 255, 255))[0]
+class ReactiveCircle:
+    def __init__(self, space, start_x, start_y, r, velocity=(0, 0), density=1, elasticity=1, collision_type=1):
+        self.r = r
+        self.body = pymunk.Body()
+        self.body.position = start_x, start_y
+        self.body.velocity = velocity
+        self.shape = pymunk.Circle(self.body, self.r)
+        self.shape.density = density
+        self.shape.mass = 1000
+        self.shape.elasticity = elasticity
+        space.add(self.body, self.shape)
+        self.shape.collision_type = collision_type
 
-original_points = [(150, 180), (170, 180), (160, 200)]
-original_center = [160, 180]
+    def draw(self, surface, color, offset):
+        pygame.draw.circle(surface, color,
+                           (int(self.body.position[0] - int(offset[0]) + 15),
+                            int(self.body.position[1] - int(offset[1]) + 15)),
+                           self.r)
 
-poly = pgeng.Polygon([(150, 180), (170, 180), (160, 200)], (255, 0, 0))
-poly2 = pgeng.Polygon([(40, 40), (120, 40), (80, 100), (80, 50)], (0, 0, 255))
-circle = pgeng.Circle(original_center, 10, (255, 0, 0))
-circle2 = pgeng.Circle([160, 80], 10, (0, 0, 255))
-rect = Rect(220, 70, 20, 20)
-rect_colour = (0, 0, 255)
 
-rotate_timer = 100
-poly_mode = True
-show_mask = False
-while True:
-    display.fill((50, 50, 50))
-    dt = pgeng.delta_time(clock, 144)
+class StaticPoly:
+    def __init__(self, space, pos, elasticity=0, collision_type=None):
+        self.pos = pos
+        self.body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        self.shape = pymunk.Poly(self.body, self.pos)
+        self.shape.elasticity = elasticity
+        if collision_type:
+            self.shape.collision_type = collision_type
+        space.add(self.body, self.shape)
 
-    keys = pygame.key.get_pressed()
-    if keys[K_a]:
-        poly.move([-1.5, 0], dt) if poly_mode else None
-        circle.center[0] -= 1.5 * dt if not poly_mode else 0
-    if keys[K_d]:
-        poly.move([1.5, 0], dt) if poly_mode else None
-        circle.center[0] += 1.5 * dt if not poly_mode else 0
-    if keys[K_s]:
-        poly.move([0, 1.5], dt) if poly_mode else None
-        circle.center[1] += 1.5 * dt if not poly_mode else 0
-    if keys[K_w]:
-        poly.move([0, -1.5], dt) if poly_mode else None
-        circle.center[1] -= 1.5 * dt if not poly_mode else 0
+    def draw(self, surface, color, offset):
+        imx, imy = offset
+        dep = [(x[0] - imx + 15, x[1] - imy + 15) for x in self.pos]
+        pygame.draw.polygon(surface, color, dep)
 
-    colliding = False
-    if poly_mode:
-        if poly.collide(poly2):
-            colliding = True
-            poly2.colour = (255, 0, 0)
-        elif poly.colliderect(rect):
-            colliding = True
-            rect_colour = (255, 0, 0)
-        elif poly.collidecircle(circle2):
-            colliding = True
-            circle2.colour = (255, 0, 0)
-        else: #so if it is not colliding with anything
-            poly2.colour = (0, 0, 255)
-            rect_colour = (0, 0, 255)
-            circle2.colour = (0, 0, 255)
-    else:
-        if circle.collide(circle2):
-            colliding = True
-            circle2.colour = (255, 0, 0)
-        elif circle.colliderect(rect):
-            colliding = True
-            rect_colour = (255, 0, 0)
-        elif circle.collidepolygon(poly2):
-            colliding = True
-            poly2.colour = (255, 0, 0)
-        else: #so if it is not colliding with anything
-            circle2.colour = (0, 0, 255)
-            rect_colour = (0, 0, 255)
-            poly2.colour = (0, 0, 255)
-    rect_colour = (255, 255, 255) if show_mask else rect_colour
 
-    rotate_timer += 0.5 * dt
-    if round(rotate_timer) <= 90:
-        poly.rotate(0.5 * dt)
-    elif poly.rotation % 90:
-        poly.rotate(pgeng.nearest(poly.rotation, 90) - poly.rotation)
+class KinematicRect:
+    def __init__(self, space, pos, collision_type=1):
+        self.pos = pos
+        self.r = 15
+        self.body = pymunk.Body()
+        self.body.position = pos[0], pos[1]
+        self.body.velocity = [0, 0]
+        self.shape = pymunk.Poly.create_box(self.body, (pos[2], pos[3]))
+        self.shape.density = 10
+        self.shape.mass = 10000000
+        self.shape.elasticity = 0
+        space.add(self.body, self.shape)
+        self.shape.collision_type = collision_type
+        self.jumping = False
 
-    poly2.render(display)
-    circle2.render(display)
-    pygame.draw.rect(display, rect_colour, rect)
-    poly.render(display) if poly_mode else circle.render(display)
-    if show_mask:
-        display.blit(poly2.mask.to_surface(), poly2.location)
-        display.blit(circle2.mask.to_surface(), circle2.location)
-        display.blit(poly.mask.to_surface(), poly.location) if poly_mode else display.blit(circle.mask.to_surface(), circle.location)
+    def just_call_me(self):
+        pass
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pgeng.quit_game()
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                pgeng.quit_game()
-            if event.key == K_SPACE:
-                poly_mode = not poly_mode
-                poly.set_points(original_points)
-                circle.center = pygame.Vector2(original_center)
-            if event.key == K_RETURN:
-                show_mask = not show_mask
-            if event.key == K_r:
-                rotate_timer = 0 if poly_mode else 91
+    def draw(self, surface, color, x, y):
+        pygame.draw.rect(surface, color, (x, y, self.pos[2], self.pos[3]))
 
-    small_font.render(display, f'{round(clock.get_fps())}', (1, 1))
-    pygame.display.update()
-    clock.tick(144)
+    def continue_jump(self, speed_y):
+        self.jumping = True
+        self.body.velocity = self.body.velocity[0], speed_y
+
+    def move(self, speed_x):
+        self.body.velocity = speed_x, self.body.velocity[1]
